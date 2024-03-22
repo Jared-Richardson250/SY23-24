b45023876b3d34433d2c83356890ef00e0f0015f
Jared <jpric137@learner.dvusd.org>
2024-03-22 12:42:18 -0700

    Added Menu Pictures

--- MotoMonkeyz/MotoMonkeyz/Form1.vb
+++ MotoMonkeyz/MotoMonkeyz/Form1.vb
@@ -45,4 +45,11 @@
         F2.ShowDialog()
         Me.Show()
     End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Me.Hide()
+        Dim F2 As New Form2
+        F2.ShowDialog()
+        Me.Show()
+    End Sub
 End Class

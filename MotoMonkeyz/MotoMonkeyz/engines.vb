2f1f61cf8e3c092ae9619ed38c05d8894fe6907f
elijah <elijahmanuma990@gmail.com>
2024-03-28 14:01:44 -0700

    added to engines form

--- MotoMonkeyz/MotoMonkeyz/engines.vb
+++ MotoMonkeyz/MotoMonkeyz/engines.vb
@@ -1,3 +1,8 @@
 ﻿Public Class engines
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim F1 As New Form1
+        F1.ShowDialog()
+        Me.Show()
+    End Sub
 End Class
